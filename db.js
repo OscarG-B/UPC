07f96c7691d3f3a7b3b48e6f655890c9220bd4b3
OscarG-B <oscar.bambaguy@gmail.com>
2021-11-05 08:19:34 -0500

    Update db.js

--- db.js
+++ db.js
@@ -30,30 +30,30 @@ const inacistenciaVacio = require('./data/Inacistencia-SinData.json');
 
 const detMatriculasIncorrecto = require('./data/DevMatriculas-Data-Incorrecto.json');
 
-const autenticarRecurso = require('./data/Autenticar-Recursos.json');
-const autenticarRecursoVacio = require('./data/Autenticar-Recursos-SinData.json');
-const autenticarRecursoError = require('./data/Autenticar-Recursos-Error.json');
+// const autenticarRecurso = require('./data/Autenticar-Recursos.json');
+// const autenticarRecursoVacio = require('./data/Autenticar-Recursos-SinData.json');
+// const autenticarRecursoError = require('./data/Autenticar-Recursos-Error.json');
 
-const recursosDisponiblesCampus = require('./data/Recursos-Disponibles-Campus.json');
-const recursosDisponiblesComputadoras = require('./data/Recursos-Disponibles-Computadoras.json');
-const recursosDisponiblesError = require('./data/Recursos-Disponibles-Error.json');
-const recursosDisponiblesVacio = require('./data/Recursos-Disponibles-SinData.json');
-const recursosDisponiblesToken = require('./data/Recursos-Disponibles-TokenVencido.json');
+// const recursosDisponiblesCampus = require('./data/Recursos-Disponibles-Campus.json');
+// const recursosDisponiblesComputadoras = require('./data/Recursos-Disponibles-Computadoras.json');
+// const recursosDisponiblesError = require('./data/Recursos-Disponibles-Error.json');
+// const recursosDisponiblesVacio = require('./data/Recursos-Disponibles-SinData.json');
+// const recursosDisponiblesToken = require('./data/Recursos-Disponibles-TokenVencido.json');
 
 
-const reservaRecurso002 = require('./data/Reservar-Recurso-0002.json');
-const reservaRecursoComputadora = require('./data/Reservar-Recurso-computador.json');
-const reservaRecursoCubiculo = require('./data/Reservar-Recurso-cubiculo.json');
-const reservaRecursoError = require('./data/Reservar-Recurso-Error.json');
+// const reservaRecurso002 = require('./data/Reservar-Recurso-0002.json');
+// const reservaRecursoComputadora = require('./data/Reservar-Recurso-computador.json');
+// const reservaRecursoCubiculo = require('./data/Reservar-Recurso-cubiculo.json');
+// const reservaRecursoError = require('./data/Reservar-Recurso-Error.json');
 
-const activarReserva = require('./data/Activar-Reserva.json');
-const activarReservaErrorCodReserva = require('./data/Activar-Reserva-Error-codReserva.json');
-const activarReservaErrorUsuAlumno = require('./data/Activar-Reserva-Error-usuAlumno.json');
-const activarReservaError = require('./data/Activar-Reserva-Error.json');
-const activarReservaNoDisponible = require('./data/Activar-Reserva-NoDisponible.json');
+// const activarReserva = require('./data/Activar-Reserva.json');
+// const activarReservaErrorCodReserva = require('./data/Activar-Reserva-Error-codReserva.json');
+// const activarReservaErrorUsuAlumno = require('./data/Activar-Reserva-Error-usuAlumno.json');
+// const activarReservaError = require('./data/Activar-Reserva-Error.json');
+// const activarReservaNoDisponible = require('./data/Activar-Reserva-NoDisponible.json');
 
-const reservaRealizadas = require('./data/ReservasRealizadas.json');
-const reservaRealizadasVacio = require('./data/ReservasRealizadas-vacio.json');
+// const reservaRealizadas = require('./data/ReservasRealizadas.json');
+// const reservaRealizadasVacio = require('./data/ReservasRealizadas-vacio.json');
 
 
 
